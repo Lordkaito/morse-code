69b3d380966e49da365743e28b558524b6062f3b
Lordkaito <cespedesisai88@gmail.com>
2021-12-09 14:47:02 +0100

    Add single char and word translation

---
+++ morse_code_translate.rb
@@ -0,0 +1,36 @@
+def translate_char_morse_code(string)
+  morse = {
+    '.-' => 'A',
+    '-...' => 'B',
+    '-.-.' => 'C',
+    '-..' => 'D',
+    '.' => 'E',
+    '..-.' => 'F',
+    '--.' => 'G',
+    '....' => 'H',
+    '..' => 'I',
+    '.---' => 'J',
+    '-.-' => 'K',
+    '.-..' => 'L',
+    '--' => 'M',
+    '-.' => 'N',
+    '---' => 'O',
+    '.--.' => 'P',
+    '--.-' => 'Q',
+    '.-.' => 'R',
+    '...' => 'S',
+    '-' => 'T',
+    '..-' => 'U',
+    '...-' => 'V',
+    '.--' => 'W',
+    '-..-' => 'X',
+    '-.--' => 'Y',
+    '--..' => 'Z'
+  }
+  morse[string]
+end
+
+def word_morse_code(string)
+  # identify words
+  string.split(' ').map { |x| translateCharMorseCode(x) }.join('')
+end
